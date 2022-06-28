58f4c73711bb1dafe73ee83299e2dfeffb3631df
nolan <623444771@qq.com>
2022-06-28 19:05:30 +0800

    1

--- Nolan.WebApi.Shared/Filters/ApiResultAttribute.cs
+++ Nolan.WebApi.Shared/Filters/ApiResultAttribute.cs
@@ -7,8 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http.Filters;
-
+ 
 namespace Nolan.WebApi.Shared.Filters
 {
     /// <summary>
